096a4d8c0913da91b08e58bb5d7c3fd5cd503c92
epitaciosilva <bessa.epitacio@gmail.com>
2020-03-05 10:15:02 -0300

    Correcting minesweeper

--- 02/minesweeper.cpp
+++ 02/minesweeper.cpp
@@ -4,15 +4,13 @@
 int main() {
 	int n,m;
 	int cont = 1;
+	int out[103][103] = {0};
 
 	while(true) {
 		std::cin >> m >> n;
 			
-		if(m == 0 && n == 0) {
-			break;
-		}
+		if(m == 0 && n == 0) break;
 
-		int out[m][n] = {0}; 
 		std::string line;
 
 		for(int i = 1; i <= m; i++) {
@@ -20,29 +18,31 @@ int main() {
 			
 			for(int j = 1; j <= n; j++) {
             	if(line[j-1] == '*') {
-					out[i][j] == -1;
+					out[i][j] = -1;
 
-					for(int k = i-1; k <=i+1; k++) {
-						for(int l = j-1; j<= j+1; l++) {
+					for(int k = i-1; k <= i+1; k++) {
+						for(int l = j-1; l<= j+1; l++) {
 							if(out[k][l] != -1) out[k][l]++;
 						}
 					}
 				}	         	
 			}
 		}
-		
-		std::cout << "Field #" << cont << ":" << std::endl;
-		for(int i = 0; i < m; i++) {
-			for(int j  = 0; j < n; j++) {
-				if(out[i][j] == -1) {
-					std::cout << "*";
-				} else {
-					std::cout << out[i][j];
-				}
-			}
-			std::cout << "\n\n";
-		}
 
+		std::cout << "Field #" << cont << ":\n";
+		for(int i = 1; i <= m+1; i++) {
+			for(int j  = 1; j <= n+1; j++) {
+				if(i < m+1 && j < n+1)  {
+					if(out[i][j] == -1) {
+						std::cout << "*";
+					} else {
+						std::cout << out[i][j];
+					}
+				}
+				out[i][j] = 0;
+			}
+			std::cout << std::endl;
+		}
 		cont++;
 	}
 
