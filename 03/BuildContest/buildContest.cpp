d12e1c47dc70741eccaff07375fe014c83f67457
epitaciosilva <bessa.epitacio>
2020-03-08 22:55:07 -0300

    Ajustes

--- 03/BuildContest/buildContest.cpp
+++ 03/BuildContest/buildContest.cpp
@@ -1,24 +1,40 @@
 #include <iostream>
 #include <cstdio>
 
-const int MAX = 10000;
-int levels, nprob = 0, verificar = 0, cont = 0;
-int probs[MAX];
+const int MAX = 100000;
+int levels, nprob = 0, start = 0, marcador = 100000, contLevels = 0, cont = 0;
+int probs[MAX], aux[MAX] = {0}, validos[MAX] = {0};
 
 bool temSolucao() {
-	int aux[levels] = {0};
-	cont = 0;
-	for(int i = verificar; i < nprob; i++) {
-		for(int j = 0; j < levels; j++) {
-			if(aux[j] != probs[i]) {
-				aux[j] = probs[i];
-				cont++;
-				break;
-			}
+	contLevels = 0;
+	
+	for(int i = start; i < nprob; i++) {		
+		// se o número ainda não está dentro de aux, adiciona
+		if(probs[i] != 0 && aux[probs[i]-1] != probs[i]) {
+			aux[probs[i]-1] = probs[i];
+			contLevels++;
+		} else if(i <= marcador){
+			// menor posição de um número válido no vetor probs
+			marcador = i;
 		}
 	}
-	verificar = nprob;
-	return true;
+	
+	// marcando probs e limpando aux
+	for(int i = start; i < nprob; i++) {
+		if(contLevels == levels && aux[probs[i]-1] != 0) {
+			aux[probs[i]-1] = 0; 
+			probs[i] = 0;
+		} else {
+			aux[probs[i]-1] = 0;
+		}
+	}
+
+	// verificando se tem solução
+	if(contLevels == levels) {	
+		start = marcador != 100000 ? marcador : nprob;
+		return true;
+	}
+	return false;
 }
 
 
@@ -29,16 +45,16 @@ int main() {
 	std::cin.tie(NULL);
 
 	std::cin >> levels >> n;
-	
 	for(; n > 0; n--) {
 		std::cin >> probs[nprob++];	
-		if(nprob-levels >= verificar) {
+		if(nprob-levels >= start) {
 			if(temSolucao()) {
 				std::cout << 1;
 			} else {
 				std::cout << 0;
 			}
 		} else {
+			validos[cont++] = nprob-1;
 			std::cout << 0;
 		}
 	}
