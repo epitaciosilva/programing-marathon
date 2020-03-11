054514ecb3d5acd9522dc1d3f90c254e521a85ba
epitaciosilva <bessa.epitacio>
2020-03-10 23:21:02 -0300

    Nova forma de fazer a build contest. Agora deu certo

--- 03/BuildContest/buildContest.cpp
+++ 03/BuildContest/buildContest.cpp
@@ -1,67 +1,34 @@
 #include <iostream>
-#include <cstdio>
-
-const int MAX = 100000;
-int levels, nprob = 0, start = 0, inicio = 0, marcador = 100000, contLevels = 0, removidos = 0;
-int probs[MAX], aux[MAX] = {0};
-
-bool temSolucao() {
-	for(int i = start; i < nprob; i++) {		
-		// se o número ainda não está dentro de aux, adiciona
-		if(probs[i] != 0 && aux[probs[i]-1] != probs[i]) {
-			aux[probs[i]-1] = probs[i];
-			contLevels++;
-		} else if(i <= marcador){
-			// menor posição de um número válido no vetor probs
-			marcador = i;
-		}
-	}
-	
-	if(contLevels != levels) {
-		start = nprob;
-		return false;
-	}
-	removidos = 0;
-	// marcando probs e limpando aux
-	for(int i = inicio; i < nprob; i++) {
-		if(aux[probs[i]-1] != 0) {
-			aux[probs[i]-1] = 0; 
-			probs[i] = 0;
-			if(i >= marcador) {
-				removidos++;
-			}
-		} else {
-			aux[probs[i]-1] = 0;
-		}
-	}
-
-	contLevels = 0;
-	start = marcador != 100000 ? marcador : nprob;
-	inicio = start;
-	return true;
-}
-
+using namespace std;
 
 int main() {
-	int n;
+	int n, m, cont = 0, nProb = 0;	
+	int probs[100001] = {};
 
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);
-
-	std::cin >> levels >> n;
-	for(; n > 0; n--) {
-		std::cin >> probs[nprob++];	
-		if(nprob >= inicio+removidos+levels) {
-			if(temSolucao()) {
-				std::cout << 1;
-			} else {
-				std::cout << 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);	
+	
+	cin >> n >> m;
+	for(; m > 0; m--) {
+		cin >> nProb;
+		if(probs[nProb] == 0) {
+			cont++;
+		}
+		probs[nProb]++;
+		if(cont == n) {
+			cont = 0;
+			for(int j = 1; j <= n; j++) {
+				probs[j]--;
+				if(probs[j] > 0) {
+					cont++;
+				}
 			}
+			cout << 1;
 		} else {
-			std::cout << 0;
+			cout << 0;
 		}
 	}
-	std::cout << std::endl;
-	
+	cout << endl;
+
 	return 0;
 }
