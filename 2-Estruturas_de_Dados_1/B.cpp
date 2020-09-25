00fffee2c64d289bb5996a53395f7b18cb442c51
Epitácio Bessa da Silva <bessa.epitacio@gmail.com>
2020-09-24 22:54:46 -0300

    Semana 2 - exercicio B

--- 2-Estruturas_de_Dados_1/B.cpp
+++ 2-Estruturas_de_Dados_1/B.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <string>
+#include <map>
+
+
 using namespace std;
 // utilizar o map do c++ (transforma string pra int)
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
+    int n;
+    map<string, int> names;
+    string name;
+
+    cin >> n;
+    while(n--) {
+        cin >> name;
+        if (names[name] == 0) {
+            names[name] += 1;
+            cout << "OK\n";
+        } else {
+            names[name + to_string(names[name])] = 1; 
+            cout << (name + to_string(names[name])) << endl; 
+            names[name] += 1;
+        } 
+    }
+
     return 0;
 }
